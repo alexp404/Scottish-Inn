9c71a6bb61e79f93ba587b68c02ec113a7a05db2
Alex Rodriguez <alex-p4-04@hotmail.com>
2026-01-06 14:42:43 -0600

    Fix link formatting in hotelLanding component

--- frontend/src/components/hotelLanding.tsx
+++ frontend/src/components/hotelLanding.tsx
@@ -351,7 +351,7 @@ const HotelLanding: React.FC = () => {
                           <svg className="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
                             <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                           </svg>
-                           <a href="https://www.google.com/maps/dir/?api=1&destination=Scottish+Inn+%26+Suites,+Houston,+TX target="_blank">
+                           <a href="https://www.google.com/maps/dir/?api=1&destination=Scottish+Inn+%26+Suites,+Houston,+TX" target="_blank">
                           2531 FM 1960 Rd, Houston, TX 77073</a>
                         </span>
                     </div>
