c1db8fe819e71f81eabd7deab7c2c5b0b134d4fc
Alex Rodriguez <alex-p4-04@hotmail.com>
2026-01-17 10:16:18 -0600

    asd

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,6 @@ export default defineConfig({
   },
   server: {
       port: 5173,
-      allowedHosts: ['.localhost', '.localdomain', '.minerals-telecommunications-banners-modems.trycloudflare.com']
+      allowedHosts: ['.localhost', '.localdomain', '.minerals-telecommunications-banners-modems.trycloudflare.com', '*.vercel.app']
   }
 })
